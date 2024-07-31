360ce68df4bf1768070a2b8d4f2eb3a7867d0fb2
shaeeb <shaeebali@gmail.com>
2024-07-31 09:28:28 +0100

    crud update users

--- Controllers/usersController.js
+++ Controllers/usersController.js
@@ -12,17 +12,30 @@ const getAllUsers = (req, res, next) => {
 
 const createNewUser = (req, res, next) => {
   const newUser = {
-    id: data.employees[data.employees.length - 1].id + 1 || 1,
+    id: data.users?.length ? data.users[data.users.length - 1].id + 1 : 1,
     firstname: req.body.firstname,
     lastname: req.body.lastname,
-  };
+    email: req.body.email,
+  }
+  if (!newUser.firstname ||!newUser.lastname ||!newUser.email) {
+    res.status(400).json({ error: 'Please provide all fields' });
+  }
+  data.setUsers([...data.users, newUser]);
+  res.status(201).json(data.users);
 };
 
 const updateUser = (req, res, next) => {
-  res.json({
-    "firstname": req.body.firstname,
-    "lastname": req.body.lastname,
-  });
+  const user = data.users.find(user => user.id === parseInt(req.body.id));
+  if (!user) {
+    res.status(400).json({ message: `User ID ${req.body.id} not found` });
+  }
+  if  (req.body.firstname) user.firstname = req.body.firstname;
+  if (req.body.lastname) user.lastname = req.body.lastname;
+  if (req.body.email) user.email = req.body.email;
+  const filteredArray = data.users.filter(user => user.id !== parseInt(req.body.id));
+  const unsortedArray = [...filteredArray, user];
+  data.setUsers(unsortedArray.sort((a, b) => a.id - b.id ? 1 : a.id < b.id ? -1 : 0));
+  res.status(200).json(data.users);
 };
 
 const deleteUser = (req, res, next) => {
