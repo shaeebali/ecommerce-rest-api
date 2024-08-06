396f215fdf656638351535d3f3f380a2539668b2
shaeeb <shaeebali@gmail.com>
2024-08-06 10:59:49 +0100

    link db to reg/users controller and test writing to database

--- Controllers/registerController.js
+++ Controllers/registerController.js
@@ -1,31 +1,24 @@
-const usersDB = {
-  users: require('../Models/users.json'),
-  setUsers: function (data) { this.users = data; }
-};
-const fsPromises = require('fs').promises;
-const path = require('path');
+const User = require('../Models/User');
 const bcrypt = require('bcrypt');
 
 const handleNewUser = async (req, res) => {
   const { username, password, email } = req.body;
   if (!username || !password || !email) return res.status(400).json({ "message": "Please provide username, password, and email" });
+  
   // check for duplicate usernames in the DB
-  const duplicateUser = usersDB.users.find(user => user.username === username);
+  const duplicateUser = await User.findOne({username: username}).exec();
   if (duplicateUser) return res.status(409).json({ "message": "Username already exists" });
+  
   try {
     // encrypt password
     const hashedPassword = await bcrypt.hash(password, 10);
-    // store the new user
-    const newUser = { 
-      "username": username,
-      "roles": { "User": 2001}, 
+    // create and store the new user
+    const result = await User.create( { 
+      "username": username, 
       "password": hashedPassword, 
       "email": email 
-    };
-    usersDB.setUsers([...usersDB.users, newUser]);
-    // write the new usersDB to the file
-    await fsPromises.writeFile(path.join(__dirname, "..", "Models", "users.json"), JSON.stringify(usersDB.users));
-    console.log(usersDB.users)
+    });
+    console.log(result);
     res.status(201).json({ "message": `User ${username} created successfully` });
 
   } catch (error) {
--- Models/user.js
+++ Models/user.js
@@ -1,76 +1,29 @@
 // create a user model class based on the database schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  roles: {
+    User: {
+      type: Number,
+      default: 2001
+    },
+    Editor: Number,
+    Admin: Number
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  refreshToken: String
+});
 
-module.exports = class User {
-  // create a new user record
-  async create(data) {
-    try {
-      const query = pgp.helpers.insert(data, null, 'users') + 'RETURNING *';
-      const result = await db.query(query);
-      
-      if (result.rows?.length) {
-        return result.rows[0];
-      } 
-        return null;
-      
-      } catch(err) {
-        throw new Error(err);
-      }
-    } 
-
-  
-  
-    async update(data) {
-      try {
-
-        const { id, ...params } = data;
-
-        const condition = pgp.as.format('WHERE id = ${id} RETURNING *', { id });
-        const statement = pgp.helpers.update(params, null, 'users') + condition;
-
-        const result = await db.query(statement);
-
-        if (result.rows?.length) {
-          return result.rows[0];
-        }
-        return null;
-
-      } catch(err) {
-        throw new Error(err);
-      }
-  };
-  
-  async findOneByEmail(email) {
-    try {
-      const query = `
-        SELECT * FROM users WHERE email = $1
-      `;
-      const result = await db.query(query, [email]);
-
-      if (result.rows?.length) {
-        return result.rows[0];
-      }
-      return null;
-
-    } catch(err) {
-        throw new Error(err);
-    }
-  };
-
-  async findOneById(id) {
-    try {
-      const query = `
-        SELECT * FROM users WHERE id = $1
-      `;
-      const result = await db.query(query, [id]);
-
-      if (result.rows?.length) {
-        return result.rows[0];
-      }
-      return null;
-
-    } catch(err) {
-      throw new Error(err);
-    }
-  }
-};
+module.exports = mongoose.model('User', userSchema);
