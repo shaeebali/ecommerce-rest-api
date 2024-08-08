57093eb1c1bece41f3d539443f0dd3ab54382b0d
shaeeb <shaeebali@gmail.com>
2024-08-08 09:55:49 +0100

    create order schema model

---
+++ Models/order.js
@@ -0,0 +1,22 @@
+// Create a order modell class based on the database schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'processing', 'shipped', 'delivered', 'cancelled'],
+    default: 'pending'
+  },
+  total: {
+    type: Decimal128,
+    required: true
+  },
+  timestamps: true,
+});
+module.exports = mongoose.model('Order', orderSchema);
