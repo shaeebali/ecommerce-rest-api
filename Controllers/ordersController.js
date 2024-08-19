cd0d70a9e4026d9d8c312ed1a0a0751441ffcf59
shaeeb <shaeebali@gmail.com>
2024-08-19 10:26:05 +0100

    add get orders feature to orderscontroller

--- Controllers/ordersController.js
+++ Controllers/ordersController.js
@@ -4,3 +4,19 @@ const Order = require('../Models/Order');
 const Cart = require('../Models/Cart');
 
 // finish this order controller, its routes and link in app.js
+// get orders
+const getOrders = async (req, res, next) => {
+  const owner = req.user._id;
+  try {
+    const order = await Order.find({ owner }).sort({ createdAt: -1 });
+    if (order) 
+      {
+        res.status(200).json(order);
+      }
+      res.status(404).json({ message: "No orders found" });
+  } catch {
+    res.status(5000).json({ message: "Internal server error" });
+  }
+};
+
+// checkout
